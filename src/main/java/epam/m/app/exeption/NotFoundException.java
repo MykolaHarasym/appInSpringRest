6b13210549e2ad1f36fc970de66b39e94701e871
shememak <Maskym_Shemet@epam.com>
2019-05-06 15:34:19 +0300

    Initial commit

---
+++ src/main/java/epam/m/app/exeption/NotFoundException.java
@@ -0,0 +1,8 @@
+package epam.m.app.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+}
